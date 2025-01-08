b6db53e28aaf12c232f425b24fcd08b6a71913f2
Dimitar Dimitrov <dimitrov.d@northeastern.edu>
2025-01-08 12:19:57 -0500

    make normalization more robust

--- aanntwin/normalize.py
+++ aanntwin/normalize.py
@@ -1,25 +1,45 @@
 """Normalization functions"""
 from typing import Dict
 from typing import List
+from typing import Optional
 
 import torch
 
 
 def normalize_values(
-    named_state_dict: Dict[str, torch.Tensor], store: Dict[str, List[torch.Tensor]]
+    named_state_dict: Dict[str, torch.Tensor],
+    store: Dict[str, List[torch.Tensor]],
+    new_refs: Optional[Dict[str, float]] = None,
+    stdevs: Optional[Dict[str, Optional[float]]] = None,
 ) -> Dict[str, torch.Tensor]:
-    """Normalize all intermediate values to a maximum of 1.0"""
+    """Normalize all intermediate values"""
 
-    max_conv2d = max(
-        val for tensor in store["conv2d"] for val in tensor.flatten().tolist()
-    )
-    max_linear = max(
-        val for tensor in store["linear"] for val in tensor.flatten().tolist()
-    )
+    if new_refs is None:
+        new_refs = {}
+    if stdevs is None:
+        stdevs = {}
 
-    named_state_dict["conv2d_bias"] /= max_conv2d
-    named_state_dict["conv2d_weight"] /= max_conv2d
-    named_state_dict["linear_bias"] /= max_linear
-    named_state_dict["linear_weight"] /= max_linear / max_conv2d
+    current_normalization = 1.0
+    for layer, tensors in store.items():
+        if (
+            f"{layer}_bias" not in named_state_dict
+            or f"{layer}_weight" not in named_state_dict
+        ):
+            continue
+        values = [value for tensor in tensors for value in tensor.flatten().tolist()]
+
+        stdev = stdevs.get(layer)
+
+        if stdev is None:
+            old_ref = max(values)
+        else:
+            raise NotImplementedError
+
+        normalization = old_ref / new_refs.get("layer", 1.0)
+
+        named_state_dict[f"{layer}_bias"] /= normalization
+        named_state_dict[f"{layer}_weight"] /= normalization / current_normalization
+
+        current_normalization *= normalization
 
     return named_state_dict
